d82e44f1144ff66fb968b9c964d991e2061e5773
nmgsantos27 <nmgsantos27@gmail.com>
2026-09-09 20:36:30 +0100

    Refactor service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,19 +1,38 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('grafisantos-v1').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'script.js',
-        'manifest.json'
-      ]);
-    })
-  );
+// Service Worker simplificado
+var CACHE = "grafisantos-v3";
+
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches.open(CACHE).then(function(cache) {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.json",
+                "./icon-192.png",
+                "./icon-512.png"
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("activate", function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(function(key) {
+                    return key !== CACHE;
+                }).map(function(key) {
+                    return caches.delete(key);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(r) {
+            return r || fetch(e.request);
+        })
+    );
 });
